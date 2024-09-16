f41be788f295e7318a2b1b4738eee1657ec6b9b9
aazhnaa <aashnarose12@gmail.com>
2024-09-16 16:38:03 +0530

    Update TTT.js

--- TTT.js
+++ TTT.js
@@ -3,12 +3,14 @@ let boxes = document.querySelectorAll(".box");
 let reset = document.querySelector("#reset");
 let winmsg = document.querySelector("#displayWinner");
 let windiv = document.querySelector(".winmsg");
-let newgame = document.querySelector(".newgame");
+let newgame = document.querySelectorAll(".newgame");
+let drawbox = document.querySelector(".drawbox");
+let drawmsg = document.querySelector("#displayDraw");
 let pX=true;
+let count = 0;
 const win=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 boxes.forEach((box) =>{
     box.addEventListener("click",()=>{
-        console.log("clciked");
         if(pX){
             box.innerText="X";
             pX=false;
@@ -19,33 +21,85 @@ boxes.forEach((box) =>{
         }
         box.disabled = true;
         checkWinner();
+        count++;
+        console.log(count);
+        if(count==9){
+            areAllBoxesChecked(boxes);
+            count = 0;
+        }
     })
 })
+function areAllBoxesChecked(boxes){
+    let flag = true;
+    for(box of boxes){
+        if(box.innerText===""){
+            flag = false;
+        }
+    }
+    console.log(flag);
+    if(!isWinnerFound && flag){
+        console.log("draw");
+        drawbox.classList.remove("hide");
+        drawmsg.innerText="THIS IS A DRAW!";
+        flag = false;
+    }
+}
+let isWinnerFound=false;
 const checkWinner = () =>{
     for(pat of win){
         if(boxes[pat[0]].innerText!="" && boxes[pat[1]].innerText!="" && boxes[pat[2]].innerText!=""){
             if(boxes[pat[0]].innerText===boxes[pat[1]].innerText && boxes[pat[1]].innerText===boxes[pat[2]].innerText){
-                winmsg.innerText=`Winner is ${boxes[pat[0]].innerText} !!!!!`;
-                windiv.classList.remove("hide");
+                declareWinner(boxes[pat[0]].innerText);
             }
         }
     }
 };
-reset.addEventListener("click",()=>{
+function declareWinner(winner){
+    console.log("win");
+    winmsg.innerText=`Winner is ${winner} !!!!!`;
+                windiv.classList.remove("hide");
+                isWinnerFound=true;
+    boxes.forEach((box)=>{
+        box.disabled=true;
+    })
+}
+
+reset.addEventListener("click",function(){
     boxes.forEach((box) => {
         box.innerText="";
         box.disabled=false;
     })
     winmsg.innerText="";
     windiv.classList.add("hide");
+    drawmsg.innerText="";
+    drawbox.classList.add("hide");
+    isWinnerFound=false;
     pX=true;
+    count=0;
 })
-newgame.addEventListener("click",()=>{
+newgame[0].addEventListener("click",function(){
     boxes.forEach((box) => {
         box.innerText="";
         box.disabled=false;
     })
     winmsg.innerText="";
     windiv.classList.add("hide");
+    drawmsg.innerText="";
+    drawbox.classList.add("hide");
+    isWinnerFound=false;
     pX=true;
+    count=0;
+})
+newgame[1].addEventListener("click",function(){
+    boxes.forEach((box) => {
+        box.innerText="";
+        box.disabled=false;
+    })
+    winmsg.innerText="";
+    windiv.classList.add("hide");
+    drawmsg.innerText="";
+    drawbox.classList.add("hide");
+    isWinnerFound=false;
+    pX=true;
+    count=0;
 })
